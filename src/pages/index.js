809f9d578e772d1e8d6322bb6eda772ee9673333
John mwanzia <johnmwanzia277@gmail.com>
2023-07-18 10:56:42 +0300

    edit hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -144,20 +144,20 @@ export default function Home() {
         >
           <div data-aos="flip-right">
             <div >
-              <p className=" text-4xl md:text-3xl lg:text-4xl px-4 lg:px-0 font-semibold  mb-8">
+              <p className=" text-4xl md:text-3xl lg:text-4xl  lg:px-0 px-4 sm:px-0 font-semibold  mb-8">
                 Hello
               </p>
-              <p className=" text-5xl md:text-4xl lg:text-6xl px-4 lg:px-0  font-bold mb-4">
+              <p className=" text-5xl md:text-4xl lg:text-6xl  lg:px-0 px-4 sm:px-0 font-bold mb-4">
                 I’m a web developer
               </p>
-              <p className=" max-w-xl  text-base font-semibold px-4 lg:px-0">
+              <p className=" max-w-xl  text-base font-semibold px-4 sm:px-0 lg:px-0">
                 with passion for creating responsive, user-friendly websites.
                 With expertise in front-end and back-end development, I deliver
                 high-quality work that exceeds client expectations.
               </p>
             </div>
 
-            <button className=" flex-1 mt-5 ml-4 lg:ml-0 py-3 pl-9 pr-10 bg-gradient-to-r from-blue-400 via-indigo-500 to-purple-600 rounded-3xl text-2xl ">
+            <button className=" flex-1 mt-5 ml-4 sm:ml-0 py-2 lg:py-3 pl-9 pr-10 bg-gradient-to-r from-blue-400 via-indigo-500 to-purple-600 rounded-3xl text-2xl ">
               Hire me
             </button>
           </div>
