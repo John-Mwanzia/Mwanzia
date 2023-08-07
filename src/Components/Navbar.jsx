0f5517b8b11799d93960e15970946e56f5d17f44
John mwanzia <johnmwanzia277@gmail.com>
2023-08-07 20:56:29 +0300

    clean up code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,78 +15,78 @@ export default function NavBar(props) {
       <div className="  inline-flex  items-center justify-between w-full xl:w-[88%] mt-7 mb-8 ">
         <Navbar isBordered className="relative">
           <Navbar.Toggle showIn="xs" />
-        <div>
-        <Navbar.Brand>
-            <Text b color="inherit">
-              <div
-                className="flex space-x-4"
-                data-aos="fade-down"
-                data-aos-delay="300"
-              >
-                <p className="text-4xl font-bold text-yellow-600">John</p>
-                <p className="text-4xl mr-4 font-bold text-red-500">Kioko</p>
-              </div>
-            </Text>
-          </Navbar.Brand>
+          <div>
+            <Navbar.Brand>
+              <Text b color="inherit">
+                <div
+                  className="flex space-x-4"
+                  data-aos="fade-down"
+                  data-aos-delay="300"
+                >
+                  <p className="text-4xl font-bold text-yellow-600">John</p>
+                  <p className="text-4xl mr-4 font-bold text-red-500">Kioko</p>
+                </div>
+              </Text>
+            </Navbar.Brand>
 
-          <Navbar.Content
-            hideIn="xs"
-            className="inline-flex space-x-12 lg:space-x-24  items-center justify-end flex-wrap "
-          >
-            <Navbar.Link
-              href="#Home"
-              data-aos-delay="400"
-              data-aos="fade-down"
-              className="text-xl"
-              onClick={() => {
-                animateScroll.scrollTo(
-                  document.getElementById("Home").offsetTop
-                );
-              }}
+            <Navbar.Content
+              hideIn="xs"
+              className="inline-flex space-x-12 lg:space-x-24  items-center justify-end flex-wrap "
             >
-              Home
-            </Navbar.Link>
-            <Navbar.Link
-              href="#About"
-              data-aos-delay="500"
-              data-aos="fade-down"
-              className="text-xl"
-              onClick={() => {
-                animateScroll.scrollTo(
-                  document.getElementById("About").offsetTop
-                );
-              }}
-            >
-              About
-            </Navbar.Link>
-            <Navbar.Link
-              href="#Services"
-              data-aos-delay="600"
-              data-aos="fade-down"
-              className="text-xl"
-              onClick={() => {
-                animateScroll.scrollTo(
-                  document.getElementById("Services").offsetTop
-                );
-              }}
-            >
-              Services
-            </Navbar.Link>
-            <Navbar.Link
-              href="#Contact"
-              data-aos-delay="700"
-              data-aos="fade-down"
-              className="text-xl"
-              onClick={() => {
-                animateScroll.scrollTo(
-                  document.getElementById("Contact").offsetTop
-                );
-              }}
-            >
-              Contact
-            </Navbar.Link>
-          </Navbar.Content>
-        </div>
+              <Navbar.Link
+                href="#Home"
+                data-aos-delay="400"
+                data-aos="fade-down"
+                className="text-xl"
+                onClick={() => {
+                  animateScroll.scrollTo(
+                    document.getElementById("Home").offsetTop
+                  );
+                }}
+              >
+                Home
+              </Navbar.Link>
+              <Navbar.Link
+                href="#About"
+                data-aos-delay="500"
+                data-aos="fade-down"
+                className="text-xl"
+                onClick={() => {
+                  animateScroll.scrollTo(
+                    document.getElementById("About").offsetTop
+                  );
+                }}
+              >
+                About
+              </Navbar.Link>
+              <Navbar.Link
+                href="#Services"
+                data-aos-delay="600"
+                data-aos="fade-down"
+                className="text-xl"
+                onClick={() => {
+                  animateScroll.scrollTo(
+                    document.getElementById("Services").offsetTop
+                  );
+                }}
+              >
+                Services
+              </Navbar.Link>
+              <Navbar.Link
+                href="#Contact"
+                data-aos-delay="700"
+                data-aos="fade-down"
+                className="text-xl"
+                onClick={() => {
+                  animateScroll.scrollTo(
+                    document.getElementById("Contact").offsetTop
+                  );
+                }}
+              >
+                Contact
+              </Navbar.Link>
+            </Navbar.Content>
+          </div>
 
           <Navbar.Collapse className="mt-4">
             {collapseItems.map((item, index) => (
