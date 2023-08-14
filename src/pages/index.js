e1fda2983041d786e2673e960f2e57d1fea2eca3
John mwanzia <johnmwanzia277@gmail.com>
2023-08-14 17:25:12 +0300

    move project section code to projects folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,49 +45,7 @@ export default function Home() {
         <HeroSection />
         <About />
         {/* Project section */}
-        <div data-aos="fade-up" className="max-w-[85%]">
-          <h1 className="text-4xl font-semibold mt-10 mb-20 text-center lg:text-left underline decoration-sky-500">
-            Projects
-          </h1>
-          <div className="flex flex-wrap justify-center items-center mt-12 gap-6  lg:gap-0">
-            <div>
-              <Link href="https://smart-tech-stores.vercel.app/">
-                <Image
-                  data-aos="zoom-in-left"
-                  src="/images/mobile.png"
-                  alt="project-pic-item1"
-                  className="xl:max-w-[360px] 2xl:max-w-[430px] xl:max-h-[305px] 2xl:max-h-[370px]"
-                  width={430}
-                  height={350}
-                />
-              </Link>
-            </div>
-            <div>
-              <Link href="https://proud-forest-09d2d6e10.3.azurestaticapps.net/">
-                <Image
-                  data-aos="zoom-in-left"
-                  src="/images/mobile1.png"
-                  alt="project-pic-item1"
-                  className="xl:max-w-[360px] 2xl:max-w-[430px] xl:max-h-[305px] 2xl:max-h-[370px]"
-                  width={430}
-                  height={350}
-                />
-              </Link>
-            </div>
-            <div>
-              <Link href="https://smart-tech-stores.vercel.app/">
-                <Image
-                  data-aos="zoom-in-left"
-                  src="/images/mobile.png"
-                  alt="project-pic-item1"
-                  className="xl:max-w-[360px] 2xl:max-w-[430px] xl:max-h-[305px] 2xl:max-h-[370px]"
-                  width={430}
-                  height={350}
-                />
-              </Link>
-            </div>
-          </div>
-        </div>
+       
         {/* Service section */}
         <div
           data-aos="fade-up"
