363bf43f7cd6d33b3d7dff671b57c9584c456eb9
John Mwanzia <johnmwanzia277@gmail.com>
2024-05-01 06:43:02 +0300

    fix: inviscible texts in some devices

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,32 +1,32 @@
-"use client";
+'use client';
 
-import Aos from "aos";
-import "aos/dist/aos.css";
-import { Menu } from "lucide-react";
-import { useEffect, useState } from "react";
-import { animateScroll } from "react-scroll";
-import { Button } from "./ui/button";
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { Menu } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { animateScroll } from 'react-scroll';
+import { Button } from './ui/button';
 
 const links = [
   {
-    name: "Home",
-    Href: "#Home",
-    id: "Home",
+    name: 'Home',
+    Href: '#Home',
+    id: 'Home',
   },
   {
-    name: "About",
-    Href: "#About",
-    id: "About",
+    name: 'About',
+    Href: '#About',
+    id: 'About',
   },
   {
-    name: " Services",
-    Href: "#Services",
-    id: "Services",
+    name: ' Services',
+    Href: '#Services',
+    id: 'Services',
   },
   {
-    name: "Contact",
-    Href: "#Contact",
-    id: "Contact",
+    name: 'Contact',
+    Href: '#Contact',
+    id: 'Contact',
   },
 ];
 
@@ -37,7 +37,7 @@ export default function Navbar() {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
     // Prevent scrolling when the menu is open
-    document.body.style.overflow = isOpen ? "auto" : "hidden";
+    document.body.style.overflow = isOpen ? 'auto' : 'hidden';
     // Reset menu animation state when closing the menu
     if (!isOpen) {
       setMenuAnimationCompleted(false);
@@ -59,62 +59,66 @@ export default function Navbar() {
 
   return (
     <>
-      <div className=" mt-7 mb-8 container px-8 2xl:px-32 overflow-x-hidden">
-        <nav className="relative flex justify-between items-center border-b-[0.5px] pb-2 border-gray-300/20">
+      <div className=" container mb-8 mt-7 overflow-x-hidden px-8 2xl:px-32">
+        <nav className="relative flex items-center justify-between border-b-8 border-red-600  pb-2">
           <div className={` z-50`}>
             <div
               className="flex space-x-4"
               data-aos="fade-down"
               data-aos-delay="300"
             >
-              <p className="text-3xl  sm:text-4xl font-bold text-yellow-600">John</p>
-              <p className="text-3xl  sm:text-4xl mr-4 font-bold text-red-500">Kioko</p>
+              <p className="text-3xl  font-bold text-yellow-600 sm:text-4xl">
+                John
+              </p>
+              <p className="mr-4  text-3xl font-bold text-red-500 sm:text-4xl">
+                Kioko
+              </p>
             </div>
           </div>
-          <div className="sm:hidden flex justify-end z-50 ">
-            <Button 
-            className="p-2 bg-yellow-500/30  "
-            onClick={toggleMenu}>
+          <div className="z-50 flex justify-end sm:hidden ">
+            <Button className="bg-yellow-500/30 p-2  " onClick={toggleMenu}>
               <Menu className="" />
             </Button>
           </div>
 
           {isOpen && (
-        <div className="sm:hidden fixed top-0 left-0 w-full h-screen bg-black bg-opacity-95 z-10">
-          <div
-            className={`flex flex-col space-y-24 items-center justify-center h-full transition-opacity duration-500 ${
-              menuAnimationCompleted ? "menu-open" : ""
-            }`}
-            onAnimationEnd={() => setMenuAnimationCompleted(true)}
-          >
-            {links.map((link, index) => (
+            <div className="fixed left-0 top-0 z-10 h-screen w-full bg-black bg-opacity-95 sm:hidden">
               <div
-                key={link.id}
-                style={{ transitionDelay: `${index * 100}ms` }}
-                className={`opacity-0 ${
-                  menuAnimationCompleted ? "opacity-100" : ""
+                className={`flex h-full flex-col items-center justify-center space-y-24 transition-opacity duration-500 ${
+                  menuAnimationCompleted ? 'menu-open' : ''
                 }`}
-                onClick={() => {
-                  animateScroll.scrollTo(
-                    document.getElementById(link.id)!.offsetTop
-                  );
-                  toggleMenu();
-                }}
+                onAnimationEnd={() => setMenuAnimationCompleted(true)}
               >
-                <p className="cursor-pointer text-white text-xl">{link.name}</p>
+                {links.map((link, index) => (
+                  <div
+                    key={link.id}
+                    style={{ transitionDelay: `${index * 100}ms` }}
+                    className={`opacity-0 ${
+                      menuAnimationCompleted ? 'opacity-100' : ''
+                    }`}
+                    onClick={() => {
+                      animateScroll.scrollTo(
+                        document.getElementById(link.id)!.offsetTop,
+                      );
+                      toggleMenu();
+                    }}
+                  >
+                    <p className="cursor-pointer text-xl text-white">
+                      {link.name}
+                    </p>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        </div>
-      )}
-          <div className=" hidden sm:flex space-x-12 lg:space-x-12  items-center justify-end flex-wrap ">
+            </div>
+          )}
+          <div className=" hidden flex-wrap items-center justify-end  space-x-12 sm:flex lg:space-x-12 ">
             <div
               data-aos-delay="400"
               data-aos="fade-down"
               className=""
               onClick={() => {
                 animateScroll.scrollTo(
-                  document.getElementById("Home")!.offsetTop
+                  document.getElementById('Home')!.offsetTop,
                 );
               }}
             >
@@ -126,7 +130,7 @@ export default function Navbar() {
               className=""
               onClick={() => {
                 animateScroll.scrollTo(
-                  document.getElementById("About")!.offsetTop
+                  document.getElementById('About')!.offsetTop,
                 );
               }}
             >
@@ -138,7 +142,7 @@ export default function Navbar() {
               className=""
               onClick={() => {
                 animateScroll.scrollTo(
-                  document.getElementById("Services")!.offsetTop
+                  document.getElementById('Services')!.offsetTop,
                 );
               }}
             >
@@ -150,7 +154,7 @@ export default function Navbar() {
               className=""
               onClick={() => {
                 animateScroll.scrollTo(
-                  document.getElementById("Contact")!.offsetTop
+                  document.getElementById('Contact')!.offsetTop,
                 );
               }}
             >
